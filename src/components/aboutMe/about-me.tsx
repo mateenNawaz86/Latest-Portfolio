cf512e5b82b272ccbed9ab5f86e897b12e731149
Mateen Nawaz <mateenNawaz86>
2025-08-17 14:53:41 +0500

    fix: add cv download functionality

--- src/components/aboutMe/about-me.tsx
+++ src/components/aboutMe/about-me.tsx
@@ -8,33 +8,45 @@ import { SectionWrapper } from "@/hoc";
 import { styles } from "@/utils/styles";
 
 const AboutMe = () => {
+  const handleDownloadCV = () => {
+    const pdfUrl = "/cv.pdf";
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "Mateen_Nawaz_CV.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
-    <div>
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        <div className="flex justify-center">
-          <Image
-            src={aboutMeVector}
-            alt="about me vector"
-            className="hidden md:block"
-          />
-        </div>
-
-        <div className="flex flex-col w-full">
-          <p className={styles.sectionSubText}>Introduction</p>
-          <h2 className={styles.sectionHeadText}>About Me</h2>
-          <p className="text-base font-medium text-white mb-[30px] text-justify">
-            {aboutMeDescription}
-          </p>
-
-          <Button buttonText="Download CV" isIcon={true} />
-        </div>
-
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className="flex justify-center">
         <Image
           src={aboutMeVector}
           alt="about me vector"
-          className="block md:hidden mt-8 w-full"
+          className="hidden md:block"
         />
       </div>
+
+      <div className="flex flex-col w-full">
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>About Me</h2>
+        <p className="text-base font-medium text-white mb-[30px] text-justify">
+          {aboutMeDescription}
+        </p>
+
+        <Button
+          buttonText="Download CV"
+          isIcon={true}
+          onClick={handleDownloadCV}
+        />
+      </div>
+
+      <Image
+        src={aboutMeVector}
+        alt="about me vector"
+        className="block md:hidden mt-8 w-full"
+      />
     </div>
   );
 };
